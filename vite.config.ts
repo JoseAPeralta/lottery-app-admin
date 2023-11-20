374898308dbd9b1347c93378fa8a634812812758
Jose A Peralta <japc1802@hotmail.com>
2023-11-20 17:47:18 -0500

    build: :heavy_plus_sign: Add vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import removeConsole from 'vite-plugin-remove-console';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { qrcode } from 'vite-plugin-qrcode';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), removeConsole(), tsconfigPaths(), qrcode()],
+});
